97ab139527c56e4d1d71d3a086b6d643818c3f5d
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:58:14 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,8 +10,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=https://raw.githubusercontent.com/RevoSucks/BMHeroRecomp/refs/heads/master/icons/app.png
 export DESKTOP=https://raw.githubusercontent.com/RevoSucks/BMHeroRecomp/refs/heads/master/.github/linux/BMHeroRecompiled.desktop
 export STARTUPWMCLASS=BMHeroRecompiled
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
